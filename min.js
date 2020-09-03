aefa871823d86d59855b8894039fb485c11b912a
JazGlimnet <20javed.zafari@glimnet.se>
2020-09-03 17:49:15 +0200

    added

---
+++ min.js
@@ -0,0 +1,9 @@
+        
+        
+        
+        
+        const resultsList = document.getElementById('results');
+        new URLSearchParams(window.location.search).forEach((value, name)=>{
+            resultsList.append(`${name}: ${value}`)
+            resultsList.append(document.createElement('br'))
+        })
